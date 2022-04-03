9c384da2d7d37735101ba593cb183e5638010edf
xovest <79008917+xovest@users.noreply.github.com>
2022-04-03 18:48:35 +0300

    lol no required

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// nothing here for now but lol the font tho
+fetch('https://randomfox.ca/floof/')
+  .then(res => res.json())
+  .then(x => {
+    console.log(x);
+    document.querySelector("body > div.card_img > img").src = x.image;
+    //cat => x.file
+    //dog => x.message
+    //bunny => x.media.poster
+    //fox => x.image
+  })
+//https://aws.random.cat/meow
+//https://dog.ceo/api/breeds/image/random
+//https://api.bunnies.io/v2/loop/random/?media=gif,png
+//https://randomfox.ca/floof/
