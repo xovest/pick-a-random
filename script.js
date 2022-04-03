f569dfdd9761ad70986dd6d1c9869662d1132d0e
xovest <79008917+xovest@users.noreply.github.com>
2022-04-03 19:01:25 +0300

    heh i did it

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-fetch('https://randomfox.ca/floof/')
-  .then(res => res.json())
-  .then(x => {
-    console.log(x);
-    document.querySelector("body > div.card_img > img").src = x.image;
-    //cat => x.file
-    //dog => x.message
-    //bunny => x.media.poster
-    //fox => x.image
-  })
-//https://aws.random.cat/meow
-//https://dog.ceo/api/breeds/image/random
-//https://api.bunnies.io/v2/loop/random/?media=gif,png
-//https://randomfox.ca/floof/
+function catter() {
+  fetch('https://aws.random.cat/meow')
+    .then(res => res.json())
+    .then(x => document.querySelector("body > div.card_img > img").src = x.file)
+}
+
+function dogger() {
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then(res => res.json())
+    .then(x => document.querySelector("body > div.card_img > img").src = x.message)
+}
+
+function bunnyer() {
+  fetch('https://api.bunnies.io/v2/loop/random/?media=gif,png')
+    .then(res => res.json())
+    .then(x => document.querySelector("body > div.card_img > img").src = x.media.poster)
+}
+
+function foxer() {
+  fetch('https://randomfox.ca/floof/')
+    .then(res => res.json())
+    .then(x => document.querySelector("body > div.card_img > img").src = x.image)
+}
+
+document.querySelector("#cat").addEventListener('click', catter)
+document.querySelector("#dog").addEventListener('click', dogger)
+document.querySelector("#bunny").addEventListener('click', bunnyer)
+document.querySelector("#fox").addEventListener('click', foxer)
